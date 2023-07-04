dbe2de6ca0ee1464e03ca721224beeb12cdcfd82
douglas85rj <douglas85rj@gmail.com>
2023-07-03 21:17:59 -0300

    ListCursos_Front

--- Front_school/src/pages/HomePage/index.jsx
+++ Front_school/src/pages/HomePage/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/auth";
 import { getCursos } from "../../services/api";
 
@@ -6,21 +6,65 @@ const HomePage = () => {
   useEffect(() => {
     (async () => {
       const response = await getCursos();
-      console.log("getCursos", response.data);
+      setCursos(response.data);
+      setLoading(false);
+
     })();
   }, []);
 
+  const [cursos, setCursos] = useState([]); // [state, setState
+  const [loading, setLoading] = useState(true); // [state, setState
   const { logout } = useContext(AuthContext);
   const handleLogout = () => {
     logout();
   };
 
+  if (loading) {
+    return <div className="loading">Carregando dados...</div>;
+  }
+
   return (
-    <div>
-      <h1>Cursos</h1>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
+    <><div>
+      <h1>Graduação e Pós-graduação</h1>
+      <nav
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+        }}
+      >
+
+        <button onClick={handleLogout}>Logout</button>
+
+      </nav>
+
+
+    </div><div>
+        <h2>Cursos disponíveis</h2>
+        <nav
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+          }}
+        >
+          {cursos.map((curso) => (
+            <div key={curso.id}>
+              <h2>{curso.nome}</h2>
+              <p>{curso.descricao}</p> 
+                       
+
+            </div>
+
+          ))}
+        </nav>
+      </div></>
+
+
   );
+  
 };
+
+
 
 export default HomePage;
